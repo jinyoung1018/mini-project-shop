8359ea0bac10f2dfd7643a19f5ae82201d49539d
jinyoung1018 <jywsb1816@gmail.com>
2023-03-28 16:57:03 +0900

    commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import {
+    createApp
+  } from 'vue'
+  
+  import App from './App.vue'
+  import router from './router'
+  import mixins from './mixins'
+  import store from './store'
+  import VueSweetalert2 from 'vue-sweetalert2';
+  import 'sweetalert2/dist/sweetalert2.min.css';
+  
+  // import 'bootstrap'
+  // import 'bootstrap/dist/css/bootstrap.min.css'
+  
+  const app = createApp(App);
+  app.use(router);
+  app.mixin(mixins);
+  app.use(store);
+  app.use(VueSweetalert2);
+  app.mount('#app');
+  
+  window.Kakao.init("dcf3a6b5bd3ae3fd38d8db09ecbe1a40");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+const target = 'http://0.0.0.0:3306';
+
+//module.exports = {
+//  devServer: {
+//    port: 8080,
+//    proxy: {
+//      '^/api': {
+//        target,
+//        changeOrigin: true
+//      },
+//      '^/upload': {
+//        target,
+//        changeOrigin: true,
+//      },
+//      '^/download': {
+//        target,
+//        changeOrigin: true,
+//      }
+//    }
+//  }
+//}
